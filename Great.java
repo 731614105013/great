2fe48e8fc4b1954450719d3829b1a0c35044a6db
731614105013 <gomathiammupk29@gmail.com>
2017-07-05 09:30:00 +0530

    Create Great.java

---
+++ Great.java
@@ -0,0 +1,27 @@
+# great
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Great
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a,b,c;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    if(a>b && a>c)
+    {
+      System.out.println("a is greater");
+      }
+      else if(b>a && b>c)
+      {
+        System.out.println("b is greater");
+        }
+        else
+        {
+          System.out.println("c is greater");
+          }
+          }
+          }
